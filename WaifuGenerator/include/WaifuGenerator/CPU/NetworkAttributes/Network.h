00fa49e447e7628c0ca86b0d8b72e0dca1793cf6
Nikita Kozlov <Nyarstot@protonmail.com>
2022-06-01 19:32:55 +0300

    Added copyrights

--- WaifuGenerator/include/WaifuGenerator/CPU/NetworkAttributes/Network.h
+++ WaifuGenerator/include/WaifuGenerator/CPU/NetworkAttributes/Network.h
@@ -1,3 +1,20 @@
+// Author: Nikita Kozlov
+// Link: github.com/nyarstot
+//
+// This file is part of WaifuGenerator neural network.
+//
+// WaifuGenerator is free software: you can redistribute it and/or modify
+// it under terms of the GNU Generan Public License as published by
+// the free software foundation, either version 3 of License, or
+// (at your option) any later version.
+//
+// WaifuGenerator is distributed in the hope that it will be usefull,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with WaifuGenerator. If not, see <https://www.gnu.org/licenses/>.
 #ifndef CPU_WAIFUGENERATOR_NETWORK_H
 #define CPU_WAIFUGENERATOR_NETWORK_H
 
--- WaifuGenerator/include/WaifuGenerator/main.cpp
+++ WaifuGenerator/include/WaifuGenerator/main.cpp
@@ -1,3 +1,21 @@
+// Author: Nikita Kozlov
+// Link: github.com/nyarstot
+//
+// This file is part of WaifuGenerator neural network.
+//
+// WaifuGenerator is free software: you can redistribute it and/or modify
+// it under terms of the GNU Generan Public License as published by
+// the free software foundation, either version 3 of License, or
+// (at your option) any later version.
+//
+// WaifuGenerator is distributed in the hope that it will be usefull,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with WaifuGenerator. If not, see <https://www.gnu.org/licenses/>.
+
 #include "waifupch.h"
 
 #include <WaifuGenerator/CPU/NetworkAttributes/Network.h>
